446a6c965ad4d5d39fcd4e1dfdd09f71405c89c9
abap34 <uchi_ymgc@her0.jp>
2023-12-29 18:43:26 +0900

    メッセージを表示するようにした

--- handler/home.go
+++ handler/home.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
-	"github.com/dacq/model"
+	"math/rand"
 	"net/http"
+	"strconv"
+
+	"github.com/dacq/model"
 )
 
 func GetHome(w http.ResponseWriter, r *http.Request) {
@@ -15,22 +18,51 @@ func GetHome(w http.ResponseWriter, r *http.Request) {
 
 	rankings := make([]Ranking, len(scores))
 	for i, score := range scores {
+		user := score.User
+		submitcount, err := model.CountSubmitLogByUser(user)
+		lastsub, err := model.GetLastSubmitTime(user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		rankings[i] = Ranking{
-			User: score.User,
-			Loss: score.Loss,
-			Rank: i + 1,
+			User:        score.User,
+			Loss:        score.Loss,
+			Rank:        i + 1,
+			SubmitCount: submitcount,
+			Last:        lastsub,
 		}
 	}
+
+	messages := []string{
+		"Kaggle班では新規班員を募集中! 詳しくは @abap34 または #team/kaggle まで！",
+		"Welcome " + getSessionUser(r) + "!",
+	}
+
+	news, err := model.GetNews(5)
+
+	for _, n := range news {
+		news_text := "【ニュース】" + n.User + "がベストスコアを更新しました！ スコア:" + strconv.FormatFloat(n.Score, 'f', 6, 64)
+		messages = append(messages, news_text)
+	}
+
+	// ランダムに並べ替え
+	rand.Shuffle(len(messages), func(i, j int) {
+		messages[i], messages[j] = messages[j], messages[i]
+	})
 
 	data := struct {
 		User     string
 		Rankings []Ranking
+		Messages []string
 	}{
 		User:     getSessionUser(r),
 		Rankings: rankings,
+		Messages: messages,
 	}
 
 	err = templates.ExecuteTemplate(w, "index.html", data)
+
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
