16cccc2723f1bceca20fb8ca31ff648802259503
abap34 <uchi_ymgc@her0.jp>
2023-12-29 18:46:22 +0900

    Update initdb.go

--- model/initdb.go
+++ model/initdb.go
@@ -5,7 +5,7 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	// "os"
+	"os"
 )
 
 var (
@@ -15,14 +15,10 @@ var (
 
 // 順位表の DB と、イベントログの DB を作成する
 func InitDB() error {
-	// user := os.Getenv("NS_MARIADB_USER")
-	// pass := os.Getenv("NS_MARIADB_PASSWORD")
-	// host := os.Getenv("NS_MARIADB_HOSTNAME")
-	// dbname := os.Getenv("NS_MARIADB_DATABASE")
-	user := "admin"
-	pass := "password"
-	host := "localhost"
-	dbname := "database"
+	user := os.Getenv("NS_MARIADB_USER")
+	pass := os.Getenv("NS_MARIADB_PASSWORD")
+	host := os.Getenv("NS_MARIADB_HOSTNAME")
+	dbname := os.Getenv("NS_MARIADB_DATABASE")
 
 	_db, err := gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", user, pass, host, dbname)+"?parseTime=True&loc=Asia%2FTokyo&charset=utf8mb4"), &gorm.Config{})
 
