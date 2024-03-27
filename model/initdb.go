c9a2a04f7fe14b99c27a966c35db4653f6630833
abap34 <uchi_ymgc@her0.jp>
2024-03-28 03:02:18 +0900

    Add logging and environment variable fallbacks in InitDB function

--- model/initdb.go
+++ model/initdb.go
@@ -6,6 +6,7 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"os"
+	"log"
 )
 
 var (
@@ -13,13 +14,24 @@ var (
 	submitlog_db  *gorm.DB
 )
 
-// 順位表の DB と、イベントログの DB を作成する
-func InitDB() error {
-	user := os.Getenv("NS_MARIADB_USER")
-	pass := os.Getenv("NS_MARIADB_PASSWORD")
-	host := os.Getenv("NS_MARIADB_HOSTNAME")
-	dbname := os.Getenv("NS_MARIADB_DATABASE")
+func getEnv(key, fallback string) string {
+    value, exists := os.LookupEnv(key)
+    if !exists {
+        value = fallback
+    }
+    return value
+}
 
+func InitDB() error {
+	user := getEnv("NS_MARIADB_USER", "admin")
+	pass := getEnv("NS_MARIADB_PASSWORD", "password")
+	host := getEnv("NS_MARIADB_HOSTNAME", "localhost")
+	dbname := getEnv("NS_MARIADB_DATABASE", "database")
+
+
+	log.Printf("user: %s, pass: %s, host: %s, dbname: %s", user, pass, host, dbname)
+
+	// リーダーボードの DB 
 	_db, err := gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", user, pass, host, dbname)+"?parseTime=True&loc=Asia%2FTokyo&charset=utf8mb4"), &gorm.Config{})
 
 	if err != nil {
@@ -29,7 +41,7 @@ func InitDB() error {
 	leaderbord_db = _db
 	leaderbord_db.AutoMigrate(&Score{})
 
-	// イベントログの DB を作成する
+	// イベントログの DB 
 	_db, err = gorm.Open(mysql.Open(fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", user, pass, host, dbname)+"?parseTime=True&loc=Asia%2FTokyo&charset=utf8mb4"), &gorm.Config{})
 	if err != nil {
 		return err
